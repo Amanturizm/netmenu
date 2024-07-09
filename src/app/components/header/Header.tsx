8fc6ca86b698d648bf5f159ca0d4816ce0617260
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2024-07-09 12:17:22 +0600

    Added adaptive for header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -6,7 +6,7 @@ import logoIcon from '@/assets/images/logo.svg';
 import profileIcon from '@/assets/images/profile.png';
 import smartPhoneIcon from '@/assets/images/smart-phone.svg';
 import { User } from '@/app/types';
-import { useLayoutEffect, useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
 const Header = () => {
   // undefined = useLayoutEffect not worked
@@ -14,16 +14,47 @@ const Header = () => {
   // User = user is signed
   const [user, setUser] = useState<User | null | undefined>(undefined);
 
+  const [isBurgerMenu, setIsBurgerMenu] = useState<boolean>(false);
+  const [burgerMenuIsActive, setBurgerMenuIsActive] = useState<boolean>(false);
+
   useLayoutEffect(() => {
     setUser(JSON.parse(localStorage.getItem('user') as string) || null);
+
+    setIsBurgerMenu(window.innerWidth <= 578 && !isBurgerMenu);
+
+    const setBurgerMenu = (e: Event) =>
+      setIsBurgerMenu((e.target as { innerWidth: number } | null)!.innerWidth <= 578 && !isBurgerMenu);
+
+    window.addEventListener('resize', setBurgerMenu);
+
+    return () => {
+      window.removeEventListener('resize', setBurgerMenu);
+    };
   }, []);
 
   return (
     <header className={[styles.header, 'wrapper'].join(' ')}>
-      <div className={styles.inner_wrapper}>
-        <nav className={styles.navbar}>
-          <Image src={logoIcon.src} width={150} height={40} alt="logo" />
-          {user !== undefined && (
+      {burgerMenuIsActive && (
+        <div className={styles.burger_content}>
+          <nav className={styles.navbar}>
+            <div className={styles.logo}>
+              <Image src={logoIcon.src} fill alt="logo" />
+            </div>
+
+            <button
+              className={[
+                styles.hamburger,
+                styles.hamburger__collapse,
+                burgerMenuIsActive ? styles.is_active : '',
+              ].join(' ')}
+              type="button"
+              onClick={() => setBurgerMenuIsActive(!burgerMenuIsActive)}
+            >
+              <span className={styles.hamburger_box}>
+                <span className={styles.hamburger_inner}></span>
+              </span>
+            </button>
+
             <div className={styles.navbar_buttons}>
               <Link href={user ? '/my-menus' : '/sign-in'}>Создать меню</Link>
               <Link href={user ? '/my-menus' : '/sign-in'} className={styles.navbar_buttons_profile}>
@@ -31,6 +62,37 @@ const Header = () => {
                 <Image src={profileIcon.src} width={20} height={20} alt="profile" />
               </Link>
             </div>
+          </nav>
+        </div>
+      )}
+      <div className={styles.inner_wrapper}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+            <Image src={logoIcon.src} fill alt="logo" />
+          </div>
+          {user !== undefined && !isBurgerMenu && (
+            <div className={styles.navbar_buttons}>
+              <Link href={user ? '/my-menus' : '/sign-in'}>Создать меню</Link>
+              <Link href={user ? '/my-menus' : '/sign-in'} className={styles.navbar_buttons_profile}>
+                {user !== null ? 'Профиль' : 'Войти'}
+                <Image src={profileIcon.src} width={20} height={20} alt="profile" />
+              </Link>
+            </div>
+          )}
+          {isBurgerMenu && (
+            <button
+              className={[
+                styles.hamburger,
+                styles.hamburger__collapse,
+                burgerMenuIsActive ? styles.is_active : '',
+              ].join(' ')}
+              type="button"
+              onClick={() => setBurgerMenuIsActive(!burgerMenuIsActive)}
+            >
+              <span className={styles.hamburger_box}>
+                <span className={styles.hamburger_inner}></span>
+              </span>
+            </button>
           )}
         </nav>
 
@@ -52,15 +114,9 @@ const Header = () => {
             </Link>
           </div>
 
-          <Image
-            className={styles.content_smart_phone}
-            src={smartPhoneIcon.src}
-            width={468}
-            height={550}
-            quality={100}
-            priority
-            alt="smart-phone"
-          />
+          <div className={styles.content_smart_phone}>
+            <Image src={smartPhoneIcon.src} fill quality={100} priority alt="smart-phone" />
+          </div>
         </div>
       </div>
     </header>
